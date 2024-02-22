a86555f2af54f9b2c2653871f77bed65e36b4ea5
Mhd2003 <mahadona2003@gmail.com>
2024-02-22 10:07:40 +0100

    ajout d etduiant

---
+++ etudiant.java
@@ -0,0 +1,16 @@
+public class etudiant {
+
+    private String addresse;
+    private String prenom ;
+    private int age;
+    private float note ;
+    private String nomecole  ;
+
+    public void calculerstat(){
+
+    }
+    
+    public void affichernom(){
+        System.out.println(prenom);
+    }
+}
